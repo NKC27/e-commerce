63d43bf57fe6b73597f680049d1e9e60b3a04fe2
NKC27 <93439579+NKC27@users.noreply.github.com>
2023-12-13 19:13:07 +0000

    payload customization of pages

--- src/payload/globals/Footer.ts
+++ src/payload/globals/Footer.ts
@@ -9,6 +9,12 @@ export const Footer: GlobalConfig = {
   },
   fields: [
     {
+      name: 'copyright',
+      label: 'Copy Right',
+      type: 'text',
+      required: true,
+    },
+    {
       name: 'navItems',
       type: 'array',
       maxRows: 6,
